ea8c3109ed8c2a5b0dd20d672645ae0b36f68545
Miguel Wicht <miguel.wicht@gmail.com>
2011-06-10 00:27:29 +0200

    - changed to background-sprites - added objects with sprite positions - changed moveEnemy so that enemy moves on the ground - removed platform
    Signed-off-by: Miguel Wicht <miguel.wicht@gmail.com>

--- resources/js/heli.js
+++ resources/js/heli.js
@@ -8,7 +8,11 @@ function main(){
 	
 	var myHeli = $('#heli'); 
 	var myOpponent = $('#opponent');
+	_board = $('#board');
+	board = {height:_board.height()-33, width:_board.width()};
 	var boardHeight = $('#board').height()-40;
+	var boardWidth = $('#board').width()-156;
+	tree = {state1:"0px 0px", state2:"156px 0px", state3: "312px 0px"};
 	var speedY = 0;
 	var gravity = 1;
 	var drag = .98;
@@ -27,11 +31,20 @@ function main(){
 	//gravity
 	var ground = true;
 	var kirbyMove = 0;
-	var	kirbyMovePath = 'url(resources/images/kirby/kirby4.png)';
-	var	kirbyMovePath1 = 'url(resources/images/kirby/kirby4.png)';
-	var	kirbyMovePath2 = 'url(resources/images/kirby/kirby5.png)';
+	kirby = {
+		down:"0px 0px", 
+		up:"-64px 0px", 
+		walk1:"-96px 0px", 
+		walk2:"-130px 0px", 
+		state3:"-128px 0px"
+	};
+
 	
 	function startGravity(){
+		console.log(kirby.state1);
+		console.log(kirby.state2);
+		console.log(kirby.state3);
+		
 		
 		var heliTop = myHeli.position().top;
 		var heliLeft = myHeli.position().left;
@@ -49,7 +62,7 @@ function main(){
 			} else {
 				speedY = 0;
 				ground = true;
-				myHeli.css("top", boardHeight-myHeli.height()); // sets heli on top of bottom
+				myHeli.css("top", board.height-myHeli.height()); // sets heli on top of bottom
 			}
 		} //end keyUp
 
@@ -80,11 +93,11 @@ function main(){
 	function kirbyWalk(){
 		if (ground) {
 			if (kirbyMove == 0) {
-				myHeli.css("background-image", 'url(resources/images/kirby/kirby4.png)');
+				myHeli.css("background-position", kirby.walk1);
 				kirbyMove++;
 			}
 			else {
-				myHeli.css("background-image", 'url(resources/images/kirby/kirby5.png)');
+				myHeli.css("background-position", kirby.walk2);
 				kirbyMove--;
 			}
 		}
@@ -99,21 +112,11 @@ function main(){
 		if(event.keyCode==key_down) keyDown = true;	
 		if(event.keyCode==key_up) {
 			keyUp = true;
-			myHeli.css("background-image", 'url(resources/images/kirby/kirby3.png)');
+			myHeli.css("background-position", kirby.up);
 		}
 		if(event.keyCode==key_right) {
 			keyRight = true;
 			myHeli.removeClass("img_flip");
-	/*		myHeli.css("background-image", kirbyMovePath);
-			if (ground) {
-				if (kirbyMove % 2 === 0) {
-					myHeli.css("background-image", kirbyMovePath);
-					
-					if (kirbyMovePath == kirbyMovePath1) kirbyMovePath = 'url(resources/images/kirby/kirby5.png)';
-					if (kirbyMovePath == kirbyMovePath2) kirbyMovePath = 'url(resources/images/kirby/kirby4.png)';
-				}
-				kirbyMove++;
-			} */
 		}
 		if(event.keyCode==key_left) {
 			keyLeft = true;
@@ -127,17 +130,18 @@ function main(){
 		if(event.keyCode==key_down) keyDown = false;	
 		if(event.keyCode==key_up) {
 			keyUp = false;
-			myHeli.css("background-image", "url(resources/images/kirby/kirby1.png)");
+			myHeli.css("background-position", kirby.down);
 		}
 		if(event.keyCode==key_right) keyRight = false;
 		if(event.keyCode==key_left) keyLeft = false;	
 	}); //end keyup
 
 
-	/******************** opponent ************************/
+	/******************** enemy ************************/
 	var direction = 2;
-	function moveOpponent() {
-		var opponentTop = myOpponent.position().top;
+	function moveEnemy(_enemy) {
+	console.log("moveEnemy");
+	/*	var opponentTop = myOpponent.position().top;
 		var opponentBottom = opponentTop + myOpponent.height();
 		
 		if (opponentBottom > boardHeight)
@@ -146,52 +150,108 @@ function main(){
 		if (opponentTop < 0) 
 			direction *= -1;
 			
-		myOpponent.css("top", opponentTop + direction);
+		myOpponent.css("top", opponentTop + direction); */
+		enemy = new Object();
+		enemy.left = _enemy.position().left;
+		enemy.top = _enemy.position().top;
+		enemy.right = _enemy.position().left + _enemy.width();
+		enemy.bottom = _enemy.position().top + _enemy.height();
+		enemy.width = _enemy.width();
+		enemy.height = _enemy.height();
 		
+		if (enemy.left < 0) {
+			direction *=-1;
+			
+		}
+		if (enemy.right > boardWidth) {
+			direction *=-1;
+			
+		}	
+		_enemy.css("left", enemy.left + direction);
 	}
 	// start gravity loop
-	var opponentLoop = setInterval(moveOpponent, 400); 
+	var enemyLoop = setInterval(function() {moveEnemy(myOpponent); }, 20); 
+	
+	
+	
+	function checkEnemyCollision(_enemy, _kirby) {
+		enemy = new Object();
+		enemy.left = _enemy.position().left;
+		enemy.top = _enemy.position().top;
+		enemy.right = _enemy.position().left + _enemy.width();
+		enemy.bottom = _enemy.position().top + _enemy.height();
+		enemy.width = _enemy.width();
+		enemy.height = _enemy.height();
+
+		kirby.left = _kirby.position().left;
+		kirby.top = _kirby.position().top;
+		kirby.right = _kirby.position().left + _kirby.width();
+		kirby.bottom = _kirby.position().top + _kirby.height();
+		kirby.width = _kirby.width();
+		kirby.height = _kirby.height();
+		
+		/* 		collisionCheck!!!!!!!					*/
+		
+		
+
+		var _platformTop = _platform.position().top;
+		var _platformLeft = _platform.position().left;
+		var _platformWidth = _platform.width();
+		var _heliTop = _heli.position().top;
+		var _heliBottom = _heli.position().top + _heli.height();
+		var _heliLeft = _heli.position().left;
+		var _heliWidth = _heli.width();
+		
+		if (_platformTop <= _heliBottom  
+			&& _platformTop >= _heliTop
+			&& _platformLeft <= _heliLeft+_heliWidth  
+			&& _platformLeft+_platformWidth >= _heliLeft/*+_heliWidth*/){
+				
+			 speedY = 0; 
+			_heli.css("top", _platformTop - _heli.height());
+			
+		}
+		
+	}
 	 
 	/******************* tree ****************************/
-	var tree = $('#tree');
+	var treeRef = $('#tree');
 	var blow = false;
-	function changeTree() {
-	//	tree.css("background-image", "url(resources/images/kirby/tree1.png)");
-			tree.css("background-position", "0px 0px");
-		console.log("changeTree()");
-		blow = false
+	function treeBreath() {
+		console.log("breath");
+		var t = setTimeout(treeBreathOut, 800);
 	}
 	
-	function treeBlow() {
-	//	tree.css("background-image", "url(resources/images/kirby/tree2.png)");
-		
-		tree.css("background-position", "156px 0px");
+	function treeBreathOut() {
+		treeRef.css("background-position", tree.state2);
+		console.log("breathOut");
 		blow = true;
-		var t = setTimeout(changeTree, 800);
-		console.log("treeBlow()");
-		
-		myHeli.css("left", myHeli.position().left-10);
-		
+		var z = setTimeout(treeBreathOutEnd, 2000);
+	}
+	
+	function treeBreathOutEnd(){
+		blow = false;
+		treeRef.css("background-position", tree.state1);
+	}
+	function treeBlow() {
+		treeRef.css("background-position", tree.state3);
+		console.log("nothing");
+		var z = setTimeout(treeBreath, 800);
 	}
 	
 	
 	function windMove() {
 		if (blow) {
-			tree.css("background-position", "311px 0px");
+			console.log("blow");
 			myHeli.css("left", myHeli.position().left-2);
 		}
 			
 	}
 	
-	var treeBlowLoop = setInterval(treeBlow, 4000); 
-
+	var treeBlowLoop = setInterval(treeBlow, 8000); 
 	var blowingWind = setInterval(windMove, 8);
 
-
-
-
-
-
+	//4000 pusten
 
 
 	/********************* platform ***************************/
@@ -219,7 +279,7 @@ function main(){
 		
 	}
 
-	var platformLoop = setInterval(function() { checkPlatform(myPlatform, myHeli); }, 50);
+//	var platformLoop = setInterval(function() { checkPlatform(myPlatform, myHeli); }, 50);
 	var movingBlock = setInterval(function() { checkPlatform(myOpponent, myHeli); }, 50);
 	
 	
