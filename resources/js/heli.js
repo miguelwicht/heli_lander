5aee0fb8cea94e29e0e8fb1fa495beba3ec5e818
Miguel Wicht <miguel.wicht@gmail.com>
2011-06-11 00:05:44 +0200

    - submitted Version + bug fix
    Signed-off-by: Miguel Wicht <miguel.wicht@gmail.com>

--- resources/js/heli.js
+++ resources/js/heli.js
@@ -4,18 +4,26 @@ $('#trace').prepend(whatToTrace+"<br/>");
 
 function main(){
 	
-//	var myHeli = $('#heli'); 
-	
-	var myOpponent = $('#opponent');
-	_board = $('#board');
-	board = {height:_board.height()-33, width:_board.width()};
-	var boardHeight = $('#board').height()-40;
-	var boardWidth = $('#board').width()-156;
-	tree = {state1:"0px 0px", state2:"-156px 0px", state3: "-312px 0px"};
+	var _board = $('#board');
+	board = {
+		id: $('#board'),
+		height: _board.height()-33, 
+		width: _board.width()
+	};
+
+	tree = {
+		id: $('#tree'),
+		blow: false,
+		state1: "0px 0px", 
+		state2:"-156px 0px", 
+		state3: "-312px 0px"
+	};
 	var speedY = 0;
 	var gravity = 1;
 	var drag = .98;
-	trace(speedY+gravity);
+	
+	var direction = 2; // enemy speed
+	
 	//key bindings
 	var key_up = "38";
 	var key_down = "40";
@@ -27,13 +35,14 @@ function main(){
 	var keyRight = false;
 	var keyLeft = false;
 	var keyDown = false;
+	
 	//gravity
 	var ground = true;
-//	var kirbyMove = 0;
+
 	
 	// Kirby SpriteStates
 	kirby = {
-		id: $('#heli'),
+		id: $('#kirby'),
 		energy: 100,
 		energyUse: 2,
 		walk: 0,
@@ -46,8 +55,19 @@ function main(){
 		state3:"-128px 0px",
 		stoneState:"-162px 0px"
 	};
+	
+	portal = {id: $('#portal')};
 
-	console.log(kirby.id);
+	
+	waddle = {
+		id: $('#waddle'),
+		walk: 0,
+		walk1: "0px 0px",
+		walk2: "-32px 0px",
+		walk3: "-62px 0px",
+		walk4: "-94px 0px"
+	}
+
 	function startGravity(){
 		kirby.top = kirby.id.position().top;
 		kirby.left = kirby.id.position().left;
@@ -70,14 +90,14 @@ function main(){
 					kirby.energy -= kirby.energyUse;
 				}
 		} else {
-			if (kirby.bottom < boardHeight){
+			if (kirby.bottom < board.height){
 				if (speedY <= 20) speedY += gravity;
 				kirby.id.css("top", kirby.top+speedY);	
 				ground = false;
 			} else {
 				speedY = 0;
 				ground = true;
-				kirby.id.css("top", board.height-kirby.id.height()); // sets heli on top of bottom
+				kirby.id.css("top", board.height-kirby.id.height()); // sets kirby on top of bottom
 				if(kirby.energy <= 80) kirby.energy += 20;
 				else kirby.energy = 100;
 			}
@@ -85,7 +105,7 @@ function main(){
 
 		if (keyDown) {						/*************** kirby stone *****************/
 			kirby.top = kirby.id.position().top;
-			kirby.id.css("top", kirby.top+10);
+		//	kirby.id.css("top", kirby.top+10);
 			kirby.stone = true;
 			kirby.id.css("background-position", kirby.stoneState);
 		}
@@ -95,18 +115,18 @@ function main(){
 		
 
 		/* horizontal movement */
-		if (keyRight){
-			kirby.left = kirby.id.position().left; // fix for left+right button bug
-			kirby.id.css("left", kirby.left+10);
-			kirby.move = true;
+		if (kirby.stone == false){
+			if (keyRight){
+				kirby.left = kirby.id.position().left; // fix for left+right button bug
+				kirby.id.css("left", kirby.left+10);
+				kirby.move = true;
+			}
+			if (keyLeft){
+				kirby.left = kirby.id.position().left; // fix for left+right button bug
+				kirby.id.css("left", kirby.left-10);
+				kirby.move = true;
+			} 
 		}
-		if (keyLeft){
-			kirby.left = kirby.id.position().left; // fix for left+right button bug
-			kirby.id.css("left", kirby.left-10);
-			kirby.move = true;
-		} 
-		
-	
 
 	}
 
@@ -115,7 +135,7 @@ function main(){
 	
 	
 	function kirbyWalk(){
-		if (kirby.move && ground) {
+		if (kirby.move && ground && kirby.stone == false) {
 			if (kirby.walk == 0) {
 				kirby.id.css("background-position", kirby.walk1);
 				kirby.walk++;
@@ -124,10 +144,37 @@ function main(){
 				kirby.id.css("background-position", kirby.walk2);
 				kirby.walk--;
 			}
-		} else if(kirby.move == false && ground) kirby.id.css("background-position", kirby.walk1);
+		} else if(kirby.move == false && ground && kirby.stone == false) kirby.id.css("background-position", kirby.walk1);
 	}
 	
 	var kirbyWalkLoop = setInterval(kirbyWalk, 300);
+	
+	
+	
+	
+	
+	/*********** waddleWalk ****************/
+	function waddleWalk(){
+		
+			if (waddle.walk == 0) {
+				if (direction > 0)
+					waddle.id.css("background-position", waddle.walk1);
+				else waddle.id.css("background-position", waddle.walk4);
+				waddle.walk++;
+			}
+			else {
+				if (direction > 0)
+					waddle.id.css("background-position", waddle.walk2);
+				else waddle.id.css("background-position", waddle.walk3);
+				waddle.walk--;
+			}
+	
+	}
+	
+	var waddleWalkLoop = setInterval(waddleWalk, 300);
+	
+	
+	
 	
 	/******************** key up+down **********************/
 	$(document).keydown(function(event){
@@ -171,19 +218,8 @@ function main(){
 
 
 	/******************** enemy ************************/
-	var direction = 2;
+	
 	function moveEnemy(_enemy) {
-	console.log("moveEnemy");
-	/*	var opponentTop = myOpponent.position().top;
-		var opponentBottom = opponentTop + myOpponent.height();
-		
-		if (opponentBottom > boardHeight)
-			direction *= -1;
-
-		if (opponentTop < 0) 
-			direction *= -1;
-			
-		myOpponent.css("top", opponentTop + direction); */
 		enemy = new Object();
 		enemy.left = _enemy.position().left;
 		enemy.top = _enemy.position().top;
@@ -192,138 +228,83 @@ function main(){
 		enemy.width = _enemy.width();
 		enemy.height = _enemy.height();
 		
-		if (enemy.left < 0) {
-			direction *=-1;
-			
-		}
-		if (enemy.right > boardWidth) {
-			direction *=-1;
-			
-		}	
+		if (enemy.left < 0 || enemy.right > board.width-150) direction *=-1;	
 		_enemy.css("left", enemy.left + direction);
 	}
 	// start gravity loop
-	var enemyLoop = setInterval(function() {moveEnemy(myOpponent); }, 20); 
+	var enemyLoop = setInterval(function() {moveEnemy(waddle.id); }, 30); 
 	
-	
-	
-	function checkEnemyCollision(_enemy, _kirby) {
-		enemy = new Object();
-		enemy.left = _enemy.position().left;
-		enemy.top = _enemy.position().top;
-		enemy.right = _enemy.position().left + _enemy.width();
-		enemy.bottom = _enemy.position().top + _enemy.height();
-		enemy.width = _enemy.width();
-		enemy.height = _enemy.height();
 
-		kirby.left = _kirby.position().left;
-		kirby.top = _kirby.position().top;
-		kirby.right = _kirby.position().left + _kirby.width();
-		kirby.bottom = _kirby.position().top + _kirby.height();
-		kirby.width = _kirby.width();
-		kirby.height = _kirby.height();
-		
-		/* 		collisionCheck!!!!!!!					*/
-		
-		
-
-		var _platformTop = _platform.position().top;
-		var _platformLeft = _platform.position().left;
-		var _platformWidth = _platform.width();
-		var _heliTop = _heli.position().top;
-		var _heliBottom = _heli.position().top + _heli.height();
-		var _heliLeft = _heli.position().left;
-		var _heliWidth = _heli.width();
-		
-		if (_platformTop <= _heliBottom  
-			&& _platformTop >= _heliTop
-			&& _platformLeft <= _heliLeft+_heliWidth  
-			&& _platformLeft+_platformWidth >= _heliLeft/*+_heliWidth*/){
-				
-			 speedY = 0; 
-			_heli.css("top", _platformTop - _heli.height());
-			
-		}
+	/*********************** tree ****************************/
 	
-		
-	}
-	 
-	/******************* tree ****************************/
-	var treeRef = $('#tree');
-	var blow = false;
 	function treeBreath() {
 		console.log("breath");
 		var t = setTimeout(treeBreathOut, 800);
 	}
 	
 	function treeBreathOut() {
-		treeRef.css("background-position", tree.state3);
-		console.log("breathOut");
-		blow = true;
+		tree.id.css("background-position", tree.state3);
+		tree.blow = true;
 		var z = setTimeout(treeBreathOutEnd, 2000);
 	}
 	
 	function treeBreathOutEnd(){
-		blow = false;
-		treeRef.css("background-position", tree.state1);
+		tree.blow = false;
+		tree.id.css("background-position", tree.state1);
 	}
 	function treeBlow() {
-		treeRef.css("background-position", tree.state2);
-		console.log("nothing");
+		tree.id.css("background-position", tree.state2);
 		var z = setTimeout(treeBreath, 800);
 	}
 	
 	
 	function windMove() {
-		if (blow && kirby.stone == false) {
-			console.log("blow");
+		if (tree.blow && kirby.stone == false) {
 			kirby.id.css("left", kirby.id.position().left-2);
-		}
-			
+		}	
 	}
 	
 	var treeBlowLoop = setInterval(treeBlow, 8000); 
 	var blowingWind = setInterval(windMove, 8);
 
-	//4000 pusten
 
 
 	/********************* platform ***************************/
-	var myPlatform = $('#platform'); 
-	
-	function checkPlatform(_platform, _heli) {
+
+	function checkPlatform(_platform, _object, _offset) {
 
 		var _platformTop = _platform.position().top;
 		var _platformLeft = _platform.position().left;
 		var _platformWidth = _platform.width();
-		var _heliTop = _heli.position().top;
-		var _heliBottom = _heli.position().top + _heli.height();
-		var _heliLeft = _heli.position().left;
-		var _heliWidth = _heli.width();
+		var _objectTop = _object.position().top;
+		var _objectBottom = _object.position().top + _object.height();
+		var _objectLeft = _object.position().left;
+		var _objectWidth = _object.width();
 		
-		if (_platformTop <= _heliBottom  
-			&& _platformTop >= _heliTop
-			&& _platformLeft <= _heliLeft+_heliWidth  
-			&& _platformLeft+_platformWidth >= _heliLeft/*+_heliWidth*/){
+		if (_platformTop+_offset <= _objectBottom  
+			&& _platformTop+_offset >= _objectTop
+			&& _platformLeft <= _objectLeft+_objectWidth  
+			&& _platformLeft+_platformWidth >= _objectLeft){
 				
 			 speedY = 0; 
-			_heli.css("top", _platformTop - _heli.height());
+			if (kirby.stone && _platform == waddle.id) direction *=-1; // change enemy direction if hit while stone
+			if (ground == false || kirby.stone == false) _object.css("top", _platformTop - _object.height() +_offset); // do not move kirby up if on the ground and stone
 			
+			if (_object == kirby.id && _platform == portal.id){
+				ground=true;
+				if(kirby.energy <= 80) kirby.energy += 20;
+				else kirby.energy = 100;
+			}
 		}
 		
 	}
 
-//	var platformLoop = setInterval(function() { checkPlatform(myPlatform, myHeli); }, 50);
-	var movingBlock = setInterval(function() { checkPlatform(myOpponent, kirby.id); }, 50);
+	var platformLoop = setInterval(function() { checkPlatform(portal.id, kirby.id, 95); }, 50);
+	var movingBlock = setInterval(function() { checkPlatform(waddle.id, kirby.id, 0); }, 50);
 	
 	
-	
-	function keepInBoundaries(){
-		if (kirby.bottom > board.height) myHeli.css("bottom", board.height);
-	}
 	
 
-	var keepInBoundariesLoop = setInterval(keepInBoundaries, 20);
 	
 	
 	
